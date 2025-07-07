f70d423c7d0c5b885c6760664f8e2929503922b8
fstmbl <florian.stembal@jhg-traunreut.de>
2025-07-07 16:49:45 +0200

    Deleted old index.html

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,14 +1,17 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 export default {
-  plugins: [svelte()],
-  root: 'src',
-  base: './',
-  build: {
-    outDir: '../docs',
-    emptyOutDir: true
-  },
-  server: {
-    open: '/index.html',
-  },
+    plugins: [svelte()],
+    root: "src",
+    base: "./",
+    build: {
+        outDir: "../docs",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: "./src/index.html", // Explicitly set the entry point
+        },
+    },
+    server: {
+        open: "/index.html",
+    },
 };
